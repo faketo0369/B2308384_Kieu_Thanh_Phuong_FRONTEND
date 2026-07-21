bcc28b4314beb3adfa44ca1adfa0254f24d21eca
KieuThanhPhuong <phuonggo33@.com>
2026-07-22 00:45:18 +0700

    Hien thi danh sach contact

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,16 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    },
   },
-});
+})
